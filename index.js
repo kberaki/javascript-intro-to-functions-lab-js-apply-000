df816d5fc045220d3a0cb3c99e8efef4bcebcfc8
kesete beraki <berakik@gmail.com>
2018-08-13 23:40:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,9 +14,10 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string===string.toLowerCase()) {
     return 'I can\'t hear you!';
-  } else if (string===string.toUpperCase()){
+  } else (string===string.toUpperCase()){
     return "YES INDEED!";
-    else (string==="I love you, Grandma") {
-      return "I love you too!"
-    }
   }
+}
+  console.log("I love you, Grandma");
+      return "I love you too!";
+    
